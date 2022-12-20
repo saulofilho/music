583f2c29e922b3e742e13651a85d25259f7db37d
saulofilho <saulosilvaf@gmail.com>
2022-12-20 18:44:40 -0300

    add button

--- src/main.js
+++ src/main.js
@@ -81,6 +81,7 @@ onblur = i => A.map((e, i) => X(i));
 // keyboard to piano key mapping
 K = `zsxdcvgbhnjm,l.;/q2w3er5t6y7ui9o0p[=]`;
 
+
 // play note on key down
 onkeydown = i => P(
   K.indexOf(i.key.toLowerCase())                  // map key to note
@@ -92,3 +93,79 @@ onkeyup = i => X(
   K.indexOf(i.key.toLowerCase())                  // map key to note
   - 5 * (K.indexOf(i.key.toLowerCase()) > 16) // overlap 2nd row of keys
 );
+
+
+
+
+
+
+const stopButton1 = document.querySelector('#stopButton1'),
+  stopButton2 = document.querySelector('#stopButton2'),
+  stopButton3 = document.querySelector('#stopButton3'),
+  stopButton4 = document.querySelector('#stopButton4'),
+  stopButton5 = document.querySelector('#stopButton5'),
+  stopButton6 = document.querySelector('#stopButton6'),
+  stopButton7 = document.querySelector('#stopButton7');
+  
+const c4 = 261.6,
+	d4 = 293.7,
+  e4 = 329.6,
+  f4 = 349.2,
+  g4 = 392.0,
+  a4 = 440.0,
+  b4 = 493.9;
+
+  	
+let context,
+	oscillator,
+  contextGain,
+  x = 1,
+  type = 'sine',
+  frequency;
+
+function start(){
+	context = new AudioContext();
+	oscillator = context.createOscillator();
+  contextGain = context.createGain();
+  oscillator.frequency.value = frequency;
+  oscillator.type = type;
+  oscillator.connect(contextGain);
+	contextGain.connect(context.destination);
+	oscillator.start(0);
+}
+
+function stop(){
+  start();
+  contextGain.gain.exponentialRampToValueAtTime(
+  	0.00001, context.currentTime + x
+	)
+}
+
+stopButton1.addEventListener('click', function(){
+	frequency = c4;
+  stop();
+});
+stopButton2.addEventListener('click', function(){
+	frequency = d4;
+  stop();
+});
+stopButton3.addEventListener('click', function(){
+	frequency = e4;
+  stop();
+});
+stopButton4.addEventListener('click', function(){
+	frequency = f4;
+  stop();
+});
+stopButton5.addEventListener('click', function(){
+	frequency = g4;
+  stop();
+});
+stopButton6.addEventListener('click', function(){
+	frequency = a4;
+  stop();
+});
+stopButton7.addEventListener('click', function(){
+	frequency = b4;
+  stop();
+});
